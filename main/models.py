813c49ad80a34fcc80040f7f304c2bdafe7a922c
Ayush Agrawal <ayush@Ayushs-MacBook-Air.local>
2020-12-31 10:21:25 +0530

    WIP

--- main/models.py
+++ main/models.py
@@ -18,20 +18,24 @@ class Competition(models.Model):
     def __str__(self):
         return self.name
 
+
 class Student_data(models.Model):
     user = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     name = models.CharField(max_length=200)
     age = models.IntegerField()
     education_level = models.IntegerField(choices=[(1,"Class 1"),(2,"Class 2"),(3,"Class 3"),(4,"Class 4"),(5,"Class 5"),(6,"Class 6"),(7,"Class 7"),(8,"Class 8"),(9,"Class 9"),(10,"Class 10"),(11,"Class 11"),(12,"Class 12"),(13,"Undergrad"),(14,"Graduate")])        
 
+
 class Expert_data(models.Model):
     user = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     name = models.CharField(max_length=200)
     age = models.IntegerField()
 
+
 class Pdf_question(models.Model):
     contest = models.ForeignKey(Competition,on_delete=models.SET_NULL,null=True,related_name='pdf_question')
     question = models.TextField(null = True,blank = True)
+
 
 class Pdf_student_response(models.Model):
     contest = models.ManyToManyField(Competition)
@@ -39,22 +43,27 @@ class Pdf_student_response(models.Model):
     fileurl = models.TextField(null = True, blank = True,default='-')
     filename = models.TextField(null = True, blank =True,default='-')    
 
+
 class MCQ_question(models.Model):
     contest = models.ForeignKey(Competition,on_delete=models.SET_NULL,null=True,related_name='mcq_question')
     question = models.TextField(null = True,blank = True)
+
 
 class MCQ_option(models.Model):
     question = models.ForeignKey(MCQ_question,on_delete=models.SET_NULL,null=True,related_name='mcq_option')    
     option = models.TextField(null = True,blank = True)
 
+
 class MCQ_correct_response(models.Model):
     question =  models.ForeignKey(MCQ_question,on_delete=models.SET_NULL,null=True,related_name='mcq_correct_question')
     answer = models.ForeignKey(MCQ_option,on_delete=models.SET_NULL,null=True,related_name='MCQ_correct_response')
+
 
 class MCQ_student_response(models.Model):
     question =  models.ForeignKey(MCQ_question,on_delete=models.SET_NULL,null=True,related_name='mcq_student_question')
     student = models.ForeignKey(User,on_delete=models.SET_NULL,null=True,related_name='mcq_student')
     response = models.ForeignKey(MCQ_option,on_delete=models.SET_NULL,null=True,related_name='MCQ_student_response') 
+
 
 class FIB_question(models.Model):
     contest = models.ForeignKey(Competition,on_delete=models.SET_NULL,null=True)
--- main/views.py
+++ main/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.core.files.storage import FileSystemStorage
-from datetime import date
+from datetime import *
 
 def home(request):
 
@@ -59,4 +59,51 @@ def compete(request):
             login(request,curr_user)
             return(redirect(home))
 
-    return render(request,'main/compete.html')  
+    now = datetime.now()
+    print(now)
+    live_competitions = Competition.objects.filter(end_time__gte=now)
+    past_competitions = Competition.objects.filter(end_time__lt=now)
+    # all_competitions = Competition.objects.all()        
+
+    return render(request,'main/compete.html',{"live_competitions":live_competitions,"past_competitions":past_competitions})  
+
+
+def competition_description(request,pk):
+
+    if request.method == 'POST':
+        if 'username' in request.POST.keys():
+            # print("Hello WOrld!")
+            curr_user = authenticate(username=request.POST['username'], password=request.POST['password'])
+            login(request,curr_user)
+            return(redirect(home))
+
+    competition = Competition.objects.get(pk=pk)   
+
+
+    return render(request,'main/competition_description.html',{"competition":competition})
+
+def competition_questions(request,pk):
+
+    if request.method == 'POST':
+        if 'username' in request.POST.keys():
+            # print("Hello WOrld!")
+            curr_user = authenticate(username=request.POST['username'], password=request.POST['password'])
+            login(request,curr_user)
+            return(redirect(home))
+
+    return render(request,'main/competition_questions.html')
+
+def competition_leaderboard(request,pk):
+
+    if request.method == 'POST':
+        if 'username' in request.POST.keys():
+            # print("Hello WOrld!")
+            curr_user = authenticate(username=request.POST['username'], password=request.POST['password'])
+            login(request,curr_user)
+            return(redirect(home))
+
+    return render(request,'main/competition_leaderboard.html')        
+
+
+
+
