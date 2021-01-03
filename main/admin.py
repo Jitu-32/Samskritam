83aa5aa40131825e230f2e36d4afe02c429b8f70
Ayush Agrawal <ayush@Ayushs-MacBook-Air.local>
2021-01-03 18:41:25 +0530

    my competitions organiser questions section done

--- main/admin.py
+++ main/admin.py
@@ -9,7 +9,7 @@ admin.site.register(Pdf_question)
 admin.site.register(Pdf_student_response)
 admin.site.register(MCQ_question)
 admin.site.register(MCQ_option)
-admin.site.register(MCQ_correct_response)
+
 admin.site.register(MCQ_student_response)
 
 admin.site.register(FIB_question)
